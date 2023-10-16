18a126f9b00a4cc3696f598f7c57b6d2b9a7e93d
Lukas11n <bolukas@icloud.com>
2023-10-16 10:44:27 +0200

    Hello World

--- Dag 5.2 - Guided project - Plan a Petting Zoo Visit/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-string[] pettingZoo =
-{
-    "alpacas", "capybaras", "chickens", "ducks", "emus", "geese",
-    "goats", "iguanas", "kangaroos", "lemurs", "llamas", "macaws",
-    "ostriches", "pigs", "ponies", "rabbits", "sheep", "tortoises",
-};
