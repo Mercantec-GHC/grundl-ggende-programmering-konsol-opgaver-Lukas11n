5c92c9e177efa286aecf913a469cd3efa1f49801
Lukas11n <bolukas@icloud.com>
2023-10-16 14:03:43 +0200

    new

---
+++ Dag 1 - Consol/Program.cs
@@ -0,0 +1,60 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
+
+/*
+string name = "bob";
+int message = 3;
+double temp = 34.4;
+
+Console.WriteLine("Hello, " + name + "! you have " + message + " messages in your inbox. The temperature is  " + temp + " celsius.");
+
+
+eller-->
+
+string name = "Bob";
+int messages = 3;
+decimal temperature = 34.4m;
+
+Console.Write("Hello, ");
+Console.Write(name);
+Console.Write("! You have ");
+Console.Write(messages);
+Console.Write(" messages in your inbox. The temperature is ");
+Console.Write(temperature);
+Console.Write(" celsius.");
+*/
+
+
+
+/*
+ * Console.WriteLine("Generating invoices for customer \"Contoso Corp\" ...\n");
+Console.WriteLine("Invoice: 1021\t\tComplete!");
+Console.WriteLine("Invoice: 1022\t\tComplete!");
+Console.WriteLine("\nOutput Directory:\t");
+*/
+
+
+
+/*
+string firstName = "Bob";
+string message = "Hello " + firstName;
+Console.WriteLine(message);
+*/
+
+
+/*
+ string message = $"{greeting} {firstName}!";
+*/
+
+
+/*
+string firstName = "Bob";
+int widgetsSold = 7;
+Console.WriteLine(firstName + " sold " + (widgetsSold + 7) + " widgets.");
+
+The parentheses symbol () becomes another overloaded operator.
+*/
+
+
+
