d012b194b8d265c4bed2de66cc7445468553d14b
Lukas11n <bolukas@icloud.com>
2023-10-17 14:45:21 +0200

    last for today

--- Dag 3 - ConsolApp/Program.cs
+++ Dag 3 - ConsolApp/Program.cs
@@ -1,2 +1,55 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+//<evaluate this condition> ? <if condition is true, return this value> : <if condition is false, return this value>
+
+/*
+int i = 0;
+while (i == 0)
+{
+    Console.WriteLine("flip heads ot tails");
+    Console.ReadLine();
+
+    Random coin = new Random();
+    int flip = coin.Next(0, 2);
+    Console.WriteLine((flip == 0) ? "heads\n" : "tails\n");
+}
+*/
+
+
+/*
+string permission = "Admin|Manager";
+Random level = new Random();
+int RandomLevel = level.Next(20, 100);
+
+if (permission.Contains("Admin"))
+{
+    if (RandomLevel > 55)
+    {
+        Console.WriteLine("Welcome, Super Admin user.");
+        Console.ReadLine();
+    }
+    else
+    {
+        Console.WriteLine("Welcome, Admin user.");
+        Console.ReadLine();
+    }
+}
+else if (permission.Contains("Manager"))
+{
+    if (RandomLevel >= 20)
+    {
+        Console.WriteLine("Contact an Admin for access.");
+        Console.ReadLine();
+    }
+    else
+    {
+        Console.WriteLine("You do not have sufficient privileges.");
+        Console.ReadLine();
+    }
+}
+else
+{
+    Console.WriteLine("You do not have sufficient privileges.");
+    Console.ReadLine();
+}
+*/
+
