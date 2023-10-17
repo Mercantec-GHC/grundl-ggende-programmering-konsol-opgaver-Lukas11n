215396d709323e89be59478cefe815c78da75bf6
Lukas11n <bolukas@icloud.com>
2023-10-17 10:45:39 +0200

    10:45

--- Dag 2 - ConsolApp/Program.cs
+++ Dag 2 - ConsolApp/Program.cs
@@ -19,6 +19,8 @@ instanciering
 
 //her lavede jeg et lille terning spil ved brug af random
 
+/*
+
 int i = 0;
 
 while (i < 1)
@@ -51,7 +53,7 @@ while (i < 1)
        Console.ReadLine();
    }
 }
-
+*/
 
 /*
 her bliver der taget brug af (Math) Math.Max i dette tilfælde for at få største verdi af de to veriabler
@@ -63,3 +65,65 @@ largerValue = Math.Max(firstValue, secondValue);
 Console.WriteLine(largerValue);
 */
 
+int i = 0;
+
+while (i == 0)
+{
+    Console.WriteLine("do you wnt to continue? y/n");
+    string svar2 = Console.ReadLine();
+    if (svar2 == "y")
+    {
+        Console.WriteLine("subscription service");
+        Console.ReadLine();
+        Console.WriteLine("when does your subscription expire?");
+        int svar = Convert.ToInt32(Console.ReadLine());
+
+        if (svar <= 10)
+        {
+            if (svar <= 5)
+            {
+                Console.WriteLine($"Your subscription expires in {svar} days.");
+                Console.WriteLine("Renew now and save 10%!");
+                Console.ReadLine();
+                if (svar <= 1)
+                {
+                    if (svar == 0)
+                    {
+                        Console.WriteLine("Your subscription has expired.");
+                        Console.ReadLine();
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Your subscription expires within a day!");
+                    Console.WriteLine("Renew now and save 20%!");
+                    Console.ReadLine();
+                }
+            }
+            else
+            {
+                Console.WriteLine("Your subscription will expire soon. Renew now!");
+                Console.ReadLine();
+            }
+        }
+        else if (svar > 10)
+        {
+            Console.WriteLine("have a nice day");
+            Console.ReadLine();
+        }
+        else
+        {
+            Console.WriteLine("invalid input");
+            Console.ReadLine();
+        }
+    }
+    else if (svar2 == "n") 
+    {
+        Console.WriteLine("closing now");
+        i++;
+    }
+    else 
+    {
+        Console.WriteLine("invalid input");
+    }
+}
